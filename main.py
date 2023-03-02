45ef4373903c692a712c5dbd6f0b095e1f042a3a
Lucas <imcvlucas@gmail.com>
2023-03-01 23:14:17 -0500

    script to open tinder webapp

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print('Hello world!')
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-if __name__ == '__main__':
-    main()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# Navigate to url
+driver.get('https://tinder.com/app/recs')
+
+time.sleep(60)
+
+# Close webdriver
+driver.quit()
