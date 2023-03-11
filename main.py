6699e7e677f36d0dcc60b2f7f4cc2b5d471da823
Lucas <imcvlucas@gmail.com>
2023-03-10 22:30:04 -0500

    style: Cleaned up code

--- main.py
+++ main.py
@@ -9,40 +9,40 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 
-# 1. Navigate to tinder web app
-URL = 'https://tinder.onelink.me/9K8a/3d4abb81'
+class tinderbot():
+    URL = 'https://tinder.onelink.me/9K8a/3d4abb81'
 
-service = ChromeService(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.get(URL)
-# driver.maximize_window()
-time.sleep(5)
+    def __init__(self):
+        # 1. Navigate to tinder web app
+        self.service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=self.service)
+        self.driver.get(self.URL)
+        # driver.maximize_window()
+        self.driver.implicitly_wait(5)
 
-# 2. Login to account
-driver.find_element(By.XPATH, '/html/body/div[2]/main/div/div/div[1]/div/div/div[3]/span/div[3]/button/div[2]/div[2]').click()
-time.sleep(5)
+    def login(self):
+        # 2. Login to account
+        self.driver.find_element(By.XPATH, '/html/body/div[2]/main/div/div/div[1]/div/div/div[3]/span/div[3]/button/div[2]/div[2]').click()
 
-# 2.1 Verify user
-try:
-    verify = driver.find_element(By.CLASS_NAME, 'sc-bdnxRM')
-    print(verify.text)
-except NoSuchElementException:
-    print('x Element not found')
-finally:
-    print('End script')
+        # 2.1 Verify user
+        try:
+            self.driver.implicitly_wait(10)
+            # verify = driver.find_element(By.CLASS_NAME, 'sc-bdnxRM')
+            # verify = self.driver.find_element(By.XPATH, '/html/body/div/div/div[1]/button')
+            verify = self.driver.find_element(By.ID, 'home_children_button')
+            print(verify.text)
+        except NoSuchElementException:
+            print('ERROR: Element not found')
+        finally:
+            print('End script')
 
-# if driver.find_element(By.CLASS_NAME, 'sc-bdnxRM kkVpCJ sc-kEqXSa'):
-#     # verify = driver.find_element(By.XPATH, '/html/body/div[2]/main/div/div[2]/button/svg')
-#     # verify = driver.find_element(By.XPATH, '/html/body/div/div/div[1]/button')
-#     # verify = driver.find_element(By.XPATH, '//*[@id="home_children_button"]')
-#     verify = driver.find_element(By.CLASS_NAME, 'sc-bdnxRM kkVpCJ sc-kEqXSa')
-#     print(verify.text)
-#     verify.click()
-#     print('3. Clicked verify button')
-# else:
-#     print('Element not found!')
+    def quit():
+        print('~~~~~ Script complete ~~~~~')
+        self.driver.quit()
 
 
-print('~~~~~ Script complete ~~~~~')
-# Close webdriver
-driver.quit()
+
+if __name__ == '__main__':
+    bot = tinderbot()
+    bot.login()
+
