8d600bb279f50eb1a6a8aeeb87e46b4bec4dfd81
Lucas <imcvlucas@gmail.com>
2023-03-12 01:20:37 -0500

    feat: Verfiying user login

--- main.py
+++ main.py
@@ -4,37 +4,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import NoSuchElementException 
+from selenium.webdriver.support import expected_conditions as EC
 
 
 
 class tinderbot():
-    URL = 'https://tinder.onelink.me/9K8a/3d4abb81'
+    # URL = 'https://tinder.onelink.me/9K8a/3d4abb81'
+    URL = 'https://tinder.com/'
 
     def __init__(self):
-        # 1. Navigate to tinder web app
         self.service = ChromeService(executable_path=ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=self.service)
         self.driver.get(self.URL)
-        # driver.maximize_window()
-        self.driver.implicitly_wait(5)
+        self.driver.maximize_window()
+
+    def close_cookie_option(self):
+        # time.sleep(3)
+        WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div[1]/div[2]/button')))
+        self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div[1]/div[2]/button').click()
 
     def login(self):
-        # 2. Login to account
-        self.driver.find_element(By.XPATH, '/html/body/div[2]/main/div/div/div[1]/div/div/div[3]/span/div[3]/button/div[2]/div[2]').click()
+        self.close_cookie_option()
 
-        # 2.1 Verify user
+        # Click login button
+        self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a').click()
+
+        # Click login with phone
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/main/div/div/div[1]/div/div/div[3]/span/div[3]/button')))
+        self.driver.find_element(By.XPATH, '/html/body/div[2]/main/div/div/div[1]/div/div/div[3]/span/div[3]/button').click()
+
+        # Verify user
         try:
-            self.driver.implicitly_wait(10)
+            WebDriverWait(self.driver, 8).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[1]/button')))
             # verify = driver.find_element(By.CLASS_NAME, 'sc-bdnxRM')
             # verify = self.driver.find_element(By.XPATH, '/html/body/div/div/div[1]/button')
-            verify = self.driver.find_element(By.ID, 'home_children_button')
-            print(verify.text)
+            # verify = self.driver.find_element(By.ID, 'home_children_button')
+            # print(verify.text)
         except NoSuchElementException:
             print('ERROR: Element not found')
-        finally:
-            print('End script')
 
     def quit():
         print('~~~~~ Script complete ~~~~~')
@@ -45,4 +54,3 @@ class tinderbot():
 if __name__ == '__main__':
     bot = tinderbot()
     bot.login()
-
